346f6e2211ed52beec3f5c72eda28de53560a566
“Matias” <cisnerosmatias394@gmail.com>
2023-08-10 13:07:08 -0300

    Comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JQuery</title>
+</head>
+<body>
+    <h1 id="elemento"></h1>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+
+    <script>
+        $('#elemento').text('Hola mundo de JQuery');
+    </script>
+</body>
+</html>
